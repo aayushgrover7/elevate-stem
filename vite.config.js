2286114e3c42c9ad9b5b3d0db8006c26f9137c64
aayushgrover7 <aayushgrover32@gmail.com>
2025-08-10 16:35:26 -0400

    Build to /docs and add CNAME + 404

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // custom domain -> root
+  base: '/',              // custom domain lives at the root
+  build: {
+    outDir: 'docs',       // 👈 GitHub Pages will read from /docs
+    emptyOutDir: true
+  }
 })
