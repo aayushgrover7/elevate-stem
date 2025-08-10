5eb886e3cb0568d76da5133a4081f4538b4456c0
aayushgrover7 <aayushgrover32@gmail.com>
2025-08-10 16:04:09 -0400

    Fix vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/', // custom domain -> root
+  base: './', // fixes broken JS/CSS paths for GitHub Pages + custom domain
 })
