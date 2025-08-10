c4afb1591c237eab8926d3865debc5ffcba632d2
aayushgrover7 <aayushgrover32@gmail.com>
2025-08-10 16:02:32 -0400

    Fix vite config and redeploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// Change base to your repo name so Pages works
 export default defineConfig({
   plugins: [react()],
-  base: '/elevate-stem/'
+  base: '/', // custom domain -> root
 })
-
