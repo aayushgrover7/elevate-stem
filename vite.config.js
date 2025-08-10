041339af9c2abe8c4b005ffe1aacdc9703c580bf
aayushgrover7 <aayushgrover32@gmail.com>
2025-08-10 15:08:07 -0400

    Add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Change base to your repo name so Pages works
 export default defineConfig({
   plugins: [react()],
+  base: '/elevate-stem/',
 })
